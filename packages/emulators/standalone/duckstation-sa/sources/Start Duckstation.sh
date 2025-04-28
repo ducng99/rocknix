697ac8aa1fa8d0d16a0fcafd306051de3c660ce9
John Williams <porschemad911@gmail.com>
2025-04-28 12:45:25 +1000

    duckstation-sa - migrate to appimage for RK3326 / RK3588 / S922X

--- packages/emulators/standalone/duckstation-sa/sources/Start Duckstation.sh
+++ packages/emulators/standalone/duckstation-sa/sources/Start Duckstation.sh
@@ -8,22 +8,23 @@ set_kill set "-9 duckstation-sa"
 
 # Filesystem vars
 IMMUTABLE_CONF_DIR="/usr/config/duckstation"
-CONF_DIR="/storage/.local/share/duckstation"
+IMMUTABLE_CONF_FILE="${IMMUTABLE_CONF_DIR}/settings.ini"
+APPIMAGE_CONF_DIR="/storage/.local/share/duckstation"
+CONF_DIR="/storage/.config/duckstation"
 CONF_FILE="${CONF_DIR}/settings.ini"
 
-#Copy config folder to .local/share/duckstation
-if [ ! -d "${CONF_DIR}" ]; then
-    mkdir -p "${CONF_DIR}"
-    cp -r "${IMMUTABLE_CONF_DIR}" "/storage/.local/share/"
-fi
+#Init config dir if required
+[ ! -d "${CONF_DIR}" ] && cp -r "${IMMUTABLE_CONF_DIR}" /storage/.config
 
-if [ ! -f "${CONF_FILE}" ]; then
-   cp ${IMMUTABLE_CONF_DIR}/settings.ini ${CONF_FILE}
-fi
+#Link config dir to where appimage can find it
+mkdir -p /storage/.local/share
+rm -rf "${APPIMAGE_CONF_DIR}"
+ln -sfv "${CONF_DIR}" "${APPIMAGE_CONF_DIR}"
+
+#Init config file if required
+[ ! -f "${CONF_FILE}" ] && cp ${IMMUTABLE_CONF_FILE} ${CONF_FILE}
 
 #Link gamecontrollerdb.txt
 ln -sf /usr/config/SDL-GameControllerDB/gamecontrollerdb.txt "${CONF_DIR}/gamecontrollerdb.txt"
-
-#sway_fullscreen "duckstation-sa" "pgrep"  &
 
 /usr/bin/duckstation-sa -fullscreen
