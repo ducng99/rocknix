8b9ddd5c85e0dd1edd2fbecf466c6d7fba6007cf
Danil Zagoskin <z@gosk.in>
2025-05-10 10:59:43 +0000

    vlc: use autotools to fix wrong aclocal version call

--- packages/multimedia/vlc/package.mk
+++ packages/multimedia/vlc/package.mk
@@ -9,6 +9,7 @@ PKG_SITE="http://www.videolan.org"
 PKG_URL="https://mirror.netcologne.de/videolan.org/${PKG_NAME}/${PKG_VERSION}/$PKG_NAME-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain libdvbpsi gnutls ffmpeg libmpeg2 zlib flac libvorbis libxml2 pulseaudio SDL2 x264 x265 aom libogg"
 PKG_LONGDESC="VideoLAN multimedia player and streamer"
+PKG_TOOLCHAIN="autotools"
 
 pre_configure_target() {
   export CFLAGS="${CFLAGS} -Wno-error=incompatible-pointer-types"
